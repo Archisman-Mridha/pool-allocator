660a0fff6d5d5312aca230e0d39ab953af1cc03d
Archisman-Mridha <archismanmridha12345@gmail.com>
2025-10-18 15:20:34 +0530

    fix: Zig build system, sinze we've upgraded to Zig v0.15.1
    Signed-off-by: Archisman-Mridha <archismanmridha12345@gmail.com>

--- build.zig
+++ build.zig
@@ -14,21 +14,34 @@ pub fn build(b: *std.Build) void {
     // Here we do not set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    // It's also possible to define more custom flags to toggle optional features
+    // of this build script using `b.option()`. All defined flags (including
+    // target and optimize options) will be listed when running `zig build --help`
+    // in this directory.
+
+    // Here we define an executable. An executable needs to have a root module
+    // which needs to expose a `main` function.
     const exe = b.addExecutable(.{
         .name = "pool-allocator",
 
-        .link_libc = true,
+        .root_module = b.createModule(.{
+            // Target and optimization levels must be explicitly wired in when
+            // defining an executable or library (in the root module), and you
+            // can also hardcode a specific target for an executable or library
+            // definition if desireable (e.g. firmware for embedded devices).
+            .target = target,
+            .optimize = optimize,
 
-        .single_threaded = false,
-        //
-        // TODO : Enable thread-sanitizer.
-        //        Currently, having some problems in MacOS (M2 chip).
-        .sanitize_thread = false,
+            .single_threaded = true,
 
-        .target = target,
-        .optimize = optimize,
+            // TODO : Enable thread-sanitizer.
+            //        Currently, having some problems in MacOS (M2 chip).
+            .sanitize_thread = false,
+
+            .link_libc = true,
+        }),
     });
-    exe.addCSourceFiles(.{
+    exe.root_module.addCSourceFiles(.{
         .files = &[_][]const u8{
             "src/main.c",
         },
@@ -38,7 +51,7 @@ pub fn build(b: *std.Build) void {
     // user invokes the `install` step (the default step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a build graph step named `run`.
+    // This creates a build graph step named `run`.
     const runBuildGraphStep = b.addRunArtifact(exe);
 
     // By making the run step depend on the install step, it will be run from the installation
@@ -53,10 +66,23 @@ pub fn build(b: *std.Build) void {
         runBuildGraphStep.addArgs(args);
     }
 
-    // This creates a build step named `run`.
-    // It will be visible in the `zig build --help` menu, and can be selected like this:
-    //		`zig build run`
-    // This will evaluate the `run` step rather than the default, which is `install`.
+    // This creates a top level step. Top level steps have a name and can be
+    // invoked by name when running `zig build` (e.g. `zig build run`).
+    // This will evaluate the `run` step rather than the default step.
+    // For a top level step to actually do something, it must depend on other
+    // steps (e.g. a Run step, as we will see in a moment).
     const runBuildStep = b.step("run", "Run the app");
     runBuildStep.dependOn(&runBuildGraphStep.step);
+
+    // Just like flags, top level steps are also listed in the `--help` menu.
+    //
+    // The Zig build system is entirely implemented in userland, which means
+    // that it cannot hook into private compiler APIs. All compilation work
+    // orchestrated by the build system will result in other Zig compiler
+    // subcommands being invoked with the right flags defined. You can observe
+    // these invocations when one fails (or you pass a flag to increase
+    // verbosity) to validate assumptions and diagnose problems.
+    //
+    // Lastly, the Zig build system is relatively simple and self-contained,
+    // and reading its source code will allow you to master it.
 }
